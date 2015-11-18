c758c8122fb5ad62f0002ffddb8bbc5e06a27479
JustisAllen <JustisAllen@me.com>
2015-11-18 01:37:19 -0800

    Got single random tip working. Need to figure out multiple.

--- app/src/main/java/csteachingtips/csteachingtinder/MainActivity.java
+++ app/src/main/java/csteachingtips/csteachingtinder/MainActivity.java
@@ -1,7 +1,11 @@
 package csteachingtips.csteachingtinder;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 import com.andtinder.view.CardContainer;
@@ -9,27 +13,64 @@ import com.andtinder.view.CardContainer;
 import java.util.Random;
 
 
-public class MainActivity extends AppCompatActivity /*implements View.OnClickListener*/ {
+public class MainActivity extends AppCompatActivity  /*implements View.OnClickListener*/ {
 
-    String[] tipList = {"First tip", "Second tip", "Third tip", "Fourth tip", "Fifth tip"};
     Button swipeButton;
     Random rand = new Random();
     CardContainer tipContainer;
     TipStackAdapter adapter;
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         adapter = new TipStackAdapter(this);
-        for (String tip : tipList) {
-            adapter.add(new Tip(tip));
-        }
         tipContainer = (CardContainer) findViewById(R.id.tips);
         tipContainer.setAdapter(adapter);
 
         swipeButton = (Button) findViewById(R.id.swipe_button);
         //swipeButton.setOnClickListener(this);
+
+        webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new TipExtractor(this), "TipExtractor");
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+
+                // Inject Javascript to extract tip
+                webView.loadUrl("javascript:window.TipExtractor.extractTip(" +
+                        "document.getElementsByClassName('tipspace')[0].innerHTML.trim());");
+            }
+        });
+
+        webView.loadUrl("http://csteachingtips.org/random-tip");
+        for (int i = 0; i < 5; i++) {
+            webView.reload();
+        }
+
     }
 
+    private class TipExtractor {
+
+        private Activity ctx;
+        public String tip_;
+
+        TipExtractor(Activity ctx) {
+            this.ctx = ctx;
+        }
+
+        @JavascriptInterface
+        public void extractTip(String tip) {
+            tip_ = tip;
+            ctx.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    adapter.add(new Tip(tip_));
+                }
+            });
+        }
+    }
 }
