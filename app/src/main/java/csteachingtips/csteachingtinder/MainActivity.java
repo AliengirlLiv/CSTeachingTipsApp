dbb89f7bfb0d7de49f9b4a0eee560911e85914ad
JustisAllen <JustisAllen@me.com>
2015-11-21 14:12:54 -0800

    Got multiple tips to load.
    Currently 5 tips load and no more after that. Need to figure out how to maintain a certain number of tips (i.e., if the number of tips falls below a threshold, then load more (which just requires reloading the webView). It's probably easiest to accomplish this by just telling the webView to reload every time a tip is liked or disliked; however, then we'd still need to get an initial number of tips, and these logics might happen in separate places which is less than ideal though not terrible. Alternatively, we might be able to add a listener to the number of tips in the stack so that whenever the number is below a threshold, another tip is loaded. This would be more complicated but have the benefit of keeping the code that maintains the number of tips in one place. In either case, we might need to pass the webView to whatever class is executing the maintenance logic so it can appropriately reload the page.
    
    Also currently when the tips are being loaded, each new tip goes on the top of the stack, which is not good. We need to figure out how to make sure it goes to the bottom so that users aren't interrupted when a new one loads.

--- app/src/main/java/csteachingtips/csteachingtinder/MainActivity.java
+++ app/src/main/java/csteachingtips/csteachingtinder/MainActivity.java
@@ -1,9 +1,8 @@
 package csteachingtips.csteachingtinder;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.webkit.JavascriptInterface;
+import android.webkit.ValueCallback;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
@@ -25,49 +24,31 @@ public class MainActivity extends AppCompatActivity  /*implements View.OnClickLi
 
         adapter = new TipStackAdapter(this);
         tipContainer = (CardContainer) findViewById(R.id.tips);
-        tipContainer.setAdapter(adapter);
 
         swipeButton = (Button) findViewById(R.id.swipe_button);
         //swipeButton.setOnClickListener(this);
 
         webView = new WebView(this);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.addJavascriptInterface(new TipExtractor(this), "TipExtractor");
         webView.setWebViewClient(new WebViewClient() {
             @Override
-            public void onPageFinished(WebView view, String url) {
+            public void onPageFinished(final WebView view, String url) {
 
                 // Inject Javascript to extract tip
-                webView.loadUrl("javascript:window.TipExtractor.extractTip(" +
-                        "document.getElementsByClassName('tipspace')[0].innerHTML.trim());");
+                view.evaluateJavascript(
+                        "document.getElementsByClassName('tipspace')[0].innerHTML.trim();",
+                        new ValueCallback<String>() {
+                            @Override
+                            public void onReceiveValue(String tip) {
+                                adapter.add(new Tip(tip));
+                                tipContainer.setAdapter(adapter);
+                                if (adapter.getCount() < 5) {
+                                    view.reload();
+                                }
+                            }
+                        });
             }
         });
-
         webView.loadUrl("http://csteachingtips.org/random-tip");
-        for (int i = 0; i < 5; i++) {
-            webView.reload();
-        }
-
-    }
-
-    private class TipExtractor {
-
-        private Activity ctx;
-        public String tip_;
-
-        TipExtractor(Activity ctx) {
-            this.ctx = ctx;
-        }
-
-        @JavascriptInterface
-        public void extractTip(String tip) {
-            tip_ = tip;
-            ctx.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    adapter.add(new Tip(tip_));
-                }
-            });
-        }
     }
 }
